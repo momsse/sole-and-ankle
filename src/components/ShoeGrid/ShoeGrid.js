aa38e7bf2c8ef3286060cd115ebc1b9b735bf3cf
momsse <1344228+momsse@users.noreply.github.com>
2021-08-20 10:20:34 +0200

    4a. grid layout

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,37 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShowCardContainer key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShowCardContainer>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: -16px;
+`;
+
+const ShowCardContainer = styled.div`
+  flex: 1 1 100%;
+  padding: 16px;
+
+  @media (min-width: 700px) {
+    flex-basis: 50%;
+  }
+  
+  @media (min-width: 1000px) {
+    flex-basis: ${100/3}%;
+  }
+
+  @media (min-width: 1200px) {
+    flex-basis: 25%;
+  }
+`
+
 
 export default ShoeGrid;
